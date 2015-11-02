08701c698aa890d28b7f0573d7812c960e535216
SamiHamid <samihamid@hotmail.co.uk>
2015-11-02 17:28:54 +0200

    Sexing Up Opening Scene

---
+++ Assets/ShrinkerScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShrinkerScript : MonoBehaviour {
+
+	public int scaleRate;
+	
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//transform.localScale.x += Vector3.one * scaleRate;
+	}
+}
